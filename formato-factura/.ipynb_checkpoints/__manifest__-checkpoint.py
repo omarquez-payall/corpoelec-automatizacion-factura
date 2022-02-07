959526724aa9254adb8de52d618599c7ff4b5c49
avpayall <avpayall@gmail.com>
2022-02-07 20:00:49 +0000

    [ADD] accounts data

--- formato-factura/.ipynb_checkpoints/__manifest__-checkpoint.py
+++ formato-factura/.ipynb_checkpoints/__manifest__-checkpoint.py
@@ -25,6 +25,7 @@
     'data': [
         'security/invoices_images_security.xml',
         'security/ir.model.access.csv',
+        'data/account.account.template.csv',
         'data/chart_data.xml',
         'data/chart_post_data.xml',
         'data/invoices_images.xml',
