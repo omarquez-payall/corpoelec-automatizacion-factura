bbfc1497a075b898e3ddaae03106200c795f6b39
avpayall <avpayall@gmail.com>
2022-02-07 19:43:47 +0000

    [ADD] fiscal localization files

--- formato-factura/.ipynb_checkpoints/__manifest__-checkpoint.py
+++ formato-factura/.ipynb_checkpoints/__manifest__-checkpoint.py
@@ -3,15 +3,14 @@
     'name': "formato-factura",
 
     'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
+        Modulo implementado para los procesos de emision de factura""",
 
     'description': """
         Long description of module's purpose
     """,
 
-    'author': "My Company",
-    'website': "http://www.yourcompany.com",
+    'author': "Payall",
+    'website': 'https://payall.com.ve/',
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
@@ -20,21 +19,29 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['account_accountant'],
+    'depends': ['base','account_accountant'],
 
     # always loaded
     'data': [
         'security/invoices_images_security.xml',
         'security/ir.model.access.csv',
+        'data/chart_data.xml',
+        'data/chart_post_data.xml',
         'data/invoices_images.xml',
+        'views/product_template_inherit_view.xml',
         'views/invoice_images.xml',
         'views/invoice_images_inherit.xml',
+        'views/account_move_inherit_view.xml',
+        'views/account_move_electricidad_inherit_view.xml',
+        'views/otros_servicios_account_move_inherit_view.xml',
         'views/res_partner_inherit.xml',
+        'views/linea_servicio_view.xml',
         'views/contract_accounts.xml',
         'report/external_layout_background_inherit.xml',
         'report/report_invoice_document.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
+        'data/product.xml'
     ],
 }
