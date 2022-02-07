109900aae429f70f86dfab4eb7008c894877586b
ander94ds <ander94ds@gmail.com>
2022-02-07 14:42:51 -0400

    [ADD] formato-factura module

---
+++ formato-factura/models/.ipynb_checkpoints/__init__-checkpoint.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+from . import invoice_images
+from . import invoice_images_inherit
+from . import res_partner
+from . import integracion_payall
+from . import contract_accounts
