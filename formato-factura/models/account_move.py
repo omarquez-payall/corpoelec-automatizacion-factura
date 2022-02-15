e93fd9e9ea6976cf3b78fa98393774e091647555
avpayall <avpayall@gmail.com>
2022-02-15 20:21:22 +0000

    [FIX] for record in self

--- formato-factura/models/account_move.py
+++ formato-factura/models/account_move.py
@@ -49,15 +49,16 @@ class AccountMove( models.Model):
 
     @api.onchange('partner_id')
     def _get_next_seq_fact(self):
-        sequence = self.env['ir.sequence'].search([('code','=', 'seq_fact')])
-        partner = self.env['res.partner'].search([('id','=',self.partner_id.id)])
-        if partner.state_id:
-            state = self.env['res.country.state'].search([('id','=',partner.state_id)])
-            next = state.short_code + sequence.get_next_char(sequence.number_next_actual)
-            return next
-        else:
-            next = sequence.get_next_char(sequence.number_next_actual)
-            return next
+        for record in self:
+            sequence = self.env['ir.sequence'].search([('code','=', 'seq_fact')])
+            partner = self.env['res.partner'].search([('id','=',record.partner_id)])
+            if partner.state_id:
+                state = self.env['res.country.state'].search([('id','=',partner.state_id)])
+                next = state.short_code + sequence.get_next_char(sequence.number_next_actual)
+                return next
+            else:
+                next = sequence.get_next_char(sequence.number_next_actual)
+                return next
     
     @api.model
     def _get_next_sequence_number_contable(self):
