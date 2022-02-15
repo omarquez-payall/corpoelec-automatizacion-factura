4e4cc25cc409250e37006b0b8362ee61b198208a
avpayall <avpayall@gmail.com>
2022-02-15 20:04:51 +0000

    [FIX] domain

--- formato-factura/models/account_move.py
+++ formato-factura/models/account_move.py
@@ -50,7 +50,7 @@ class AccountMove( models.Model):
     @api.model
     def _get_next_seq_fact(self):
         sequence = self.env['ir.sequence'].search([('code','=', 'seq_fact')])
-        partner = self.env['res.partner'].search([('id','=',self.partner_id)])
+        partner = self.env['res.partner'].search([('id','=',self.partner_id.id)])
         if partner.state_id:
             state = self.env['res.country.state'].search([('id','=',partner.state_id)])
             next = state.short_code + sequence.get_next_char(sequence.number_next_actual)
