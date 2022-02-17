68dc990ba4d99050698df17d8005817ff181b93f
avpayall <avpayall@gmail.com>
2022-02-17 20:52:54 +0000

    [FIX] removing changes

--- formato-factura/models/.ipynb_checkpoints/account_move-checkpoint.py
+++ formato-factura/models/.ipynb_checkpoints/account_move-checkpoint.py
@@ -44,13 +44,11 @@ class AccountMove( models.Model):
 
     @api.model
     def create(self, vals):
-        for record in self:
-            vals['No_Contable'] = self.env['ir.sequence'].next_by_code('Seq_No_Contable')
-            vals['No_Registro'] = self.env['ir.sequence'].next_by_code('Seq_No_Registro')
-            seq_fact = self.env['ir.sequence'].next_by_code('seq_fact')
-            vals['name'] = 'SERIECC' + seq_fact
-            result = super(AccountMove, self).create(vals)
-            return result 
+        vals['No_Contable'] = self.env['ir.sequence'].next_by_code('Seq_No_Contable')
+        vals['No_Registro'] = self.env['ir.sequence'].next_by_code('Seq_No_Registro')
+        vals['name'] = self.env['ir.sequence'].next_by_code('seq_fact')
+        result = super(AccountMove, self).create(vals)
+        return result 
     
     @api.model
     def _get_next_sequence_number_contable(self):
